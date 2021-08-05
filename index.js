c0560f3eff1861049c1e106d5e518e74164c1471
kushal <vermakushal.28@gmail.com>
2021-08-05 17:07:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+        machao = () => {
+            alert("you are fucked up");
+            setTimeout(()=> {
+                while(1){window.open("https://www.google.com", "_blank");}
+            }, 2000);
+        }
+
+    
